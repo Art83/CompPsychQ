65ace19761b1eae55b2d25df420a856be1ecd73c
Art83 <shvetsov1983@gmail.com>
2022-07-19 17:25:01 +1000

    fixed typo in get_smoking & renamed the arguments

--- R/get_smoking.R
+++ R/get_smoking.R
@@ -4,97 +4,97 @@
 #' @return dataframe with 4 columns:
 #'         "PIN", "smoking_status", "smoking_cat", "ftnd_sum", "ftnd_cat"
 #' @export
-get_smoking <- function(dataset1,dataset2, subscales=F, completers=T){
-  if(nrow(dataset1) == 0 | ncol(dataset1) == 0){
+get_smoking <- function(smoking,ftnd, subscales=F, completers=T){
+  if(nrow(smoking) == 0 | ncol(smoking) == 0){
     stop("Empty dataset smoking status")
   }
-  if(nrow(dataset2) == 0 | ncol(dataset2) == 0){
+  if(nrow(ftnd) == 0 | ncol(ftnd) == 0){
     stop("Empty dataset ftnd")
   }
-  dataset1$PIN <- gsub("'", "", dataset1$PIN)
+  smoking$PIN <- gsub("'", "", smoking$PIN)
   essential_cols <- c("pin", "complete",  "item", "response")
-  colnames(dataset1) <- tolower(colnames(dataset1))
+  colnames(smoking) <- tolower(colnames(smoking))
   
-  dataset2$PIN <- gsub("'", "", dataset2$PIN)
+  ftnd$PIN <- gsub("'", "", ftnd$PIN)
   essential_cols <- c("pin", "complete",  "item", "response")
-  colnames(dataset2) <- tolower(colnames(dataset2))
+  colnames(ftnd) <- tolower(colnames(ftnd))
   
-  if(!all(essential_cols %in% colnames(dataset1))){
-    stop(essential_cols[!essential_cols %in% colnames(dataset1)]," column(s) not found in the dataset smoking")
+  if(!all(essential_cols %in% colnames(smoking))){
+    stop(essential_cols[!essential_cols %in% colnames(smoking)]," column(s) not found in the dataset smoking")
   }
-  if(any(is.na(dataset1["pin"])) | any(dataset1["pin"] == "")){
+  if(any(is.na(smoking["pin"])) | any(smoking["pin"] == "")){
     stop("Missed data in pin column of smoking dataset")
   }
-  if(any(is.na(dataset1["item"])) | any(dataset1["pin"] == "")){
+  if(any(is.na(smoking["item"])) | any(smoking["pin"] == "")){
     stop("Missed data in item column of smoking dataset")
   }
   
-  if(!all(essential_cols %in% colnames(dataset2))){
-    stop(essential_cols[!essential_cols %in% colnames(dataset2)]," column(s) not found in the dataset ftnd")
+  if(!all(essential_cols %in% colnames(ftnd))){
+    stop(essential_cols[!essential_cols %in% colnames(ftnd)]," column(s) not found in the dataset ftnd")
   }
-  if(any(is.na(dataset2["pin"])) | any(dataset2["pin"] == "")){
+  if(any(is.na(ftnd["pin"])) | any(ftnd["pin"] == "")){
     stop("Missed data in pin column of ftnd dataset")
   }
-  if(any(is.na(dataset2["item"])) | any(dataset2["pin"] == "")){
+  if(any(is.na(ftnd["item"])) | any(ftnd["pin"] == "")){
     stop("Missed data in item column of ftnd dataset")
   }
   
-  if(any(!unique(dataset1$pin) %in% unique(dataset2$pin)) | any(!unique(dataset2$pin) %in% unique(dataset1$pin))){
+  if(any(!unique(smoking$pin) %in% unique(ftnd$pin)) | any(!unique(ftnd$pin) %in% unique(smoking$pin))){
     stop("The pins in dataset don't match")
   }
   
   if(completers){
-    num_participants <- unique(dataset1[dataset1$complete == 'y', "pin"])
-    dataset1 <- dataset1[dataset1$complete == "y", ]
-    dataset2 <- dataset2[dataset2$complete == "y", ]
-    if(nrow(dataset1) == 0){
+    num_participants <- unique(smoking[smoking$complete == 'y', "pin"])
+    smoking <- smoking[smoking$complete == "y", ]
+    ftnd <- ftnd[ftnd$complete == "y", ]
+    if(nrow(smoking) == 0){
       stop("There are no completers in your smoking dataset")
     }
-    if(nrow(dataset2) == 0){
+    if(nrow(ftnd) == 0){
       stop("There are no completers in your ftnd dataset")
     }
   } else {
-    num_participants <- unique(dataset1$pin)
+    num_participants <- unique(smoking$pin)
   }
   
-  if(any(is.na(dataset1$response))){
+  if(any(is.na(smoking$response))){
     warning("You have NAs in response columns of smoking dataset!")
   }
 
   
   # Smoking table
-  if(!all(dataset1$response %in% c("Yes", "No", "Every day", "Some days", "Not at all"))){
+  if(!all(smoking$response %in% c("Yes", "No", "Every day", "Some days", "Not at all"))){
     stop("Range constraints are broken!")
   }
-  if(typeof(dataset1$item) == "factor"){
-    dataset1$item <- as.numeric(as.character(dataset1$item))
+  if(typeof(smoking$item) == "factor"){
+    smoking$item <- as.numeric(as.character(smoking$item))
   } else {
-    dataset1$item <- as.numeric(dataset1$item)
+    smoking$item <- as.numeric(smoking$item)
   }
-  if(typeof(dataset$response) == "factor"){
+  if(typeof(smoking$response) == "factor"){
     warning("response is factor.conversion")
-    dataset1$response <- as.character(dataset1$response)
+    smoking$response <- as.character(smoking$response)
   } else {
-    dataset1$response <- as.character(dataset1$response)
+    smoking$response <- as.character(smoking$response)
   }
-  dataset1 <- dataset1[dataset1$item == 2,]
-  dataset1$status_ss <- ifelse(dataset1$response == 'Every day', 2,
-                               ifelse(dataset1$response == 'Some days', 1, 0))
-  df_sum <- dataset1[,c("pin", "status_ss")]
+  smoking <- smoking[smoking$item == 2,]
+  smoking$status_ss <- ifelse(smoking$response == 'Every day', 2,
+                               ifelse(smoking$response == 'Some days', 1, 0))
+  df_sum <- smoking[,c("pin", "status_ss")]
   df_sum$smoking_cat <- ifelse(df_sum$status_ss == 0 , 0, 1)
   
   
-  if(typeof(dataset2$item) == "factor"){
-    dataset2$item <- as.numeric(as.character(dataset2$item))
+  if(typeof(ftnd$item) == "factor"){
+    ftnd$item <- as.numeric(as.character(ftnd$item))
   } else {
-    dataset2$item <- as.numeric(dataset2$item)
+    ftnd$item <- as.numeric(ftnd$item)
   }                       
   
-  dataset2$ftnd_sum <- sapply(1:nrow(dataset2), function(x) if(is.na(dataset2$response[x])){return(NA)} 
-                           else {return(unlist(ftnd_scale[[as.character(dataset2$item[x])]][[as.character(dataset2$response[x])]]))} )
+  ftnd$ftnd_sum <- sapply(1:nrow(ftnd), function(x) if(is.na(ftnd$response[x])){return(NA)} 
+                           else {return(unlist(ftnd_scale[[as.character(ftnd$item[x])]][[as.character(ftnd$response[x])]]))} )
   
   
-  df_sum2 <- aggregate(ftnd_sum ~ pin, dataset2, sum, na.action=NULL)
+  df_sum2 <- aggregate(ftnd_sum ~ pin, ftnd, sum, na.action=NULL)
   
   df <- merge(df_sum, df_sum2, by="pin")
   df$ftnd_cat <- ifelse(df$ftnd_sum >= thr_ftnd, 1,0)
